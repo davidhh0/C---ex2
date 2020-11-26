8e0c0a43bd32d2d764703122e6a0eb72526ceb10
davidhh0 <74098365+davidhh0@users.noreply.github.com>
2020-11-26 15:38:43 +0200

    Update myBank.c

--- myBank.c
+++ myBank.c
@@ -15,27 +15,26 @@ accounts[x][1] -> stores the money
 
 int main()
 {
-   
+
     char whatFunction = '1';
     // printf("Enter a function:");
     //whatFunction = getchar();
     int *p_numOfAccounts;
-    
 
-    double (*p_accounts)[MAX][2] = &accounts;
+    double(*p_accounts)[MAX][2] = &accounts;
 
     p_numOfAccounts = &numOfAccounts;
 
     while (whatFunction != 'E' && whatFunction != EOF)
     {
-        whatFunction='1';    
-        printf("Enter a function: ");
-       scanf(" %c", &whatFunction);
-       // whatFunction = getchar();
+        printf("Please choose a transaction type:\nO-Open Account\nB-Balance Inquiry\nD-Deposit\nW-Withdrawal\nC-Close Account\nI-Interest\nP-Print\nE-Exit\n");
+
+        scanf(" %c", &whatFunction);
+        // whatFunction = getchar();
         switch (whatFunction)
         {
         case 'O':
-            O(p_accounts,p_numOfAccounts);
+            O(p_accounts, p_numOfAccounts);
             break;
         case 'B':
             B(p_accounts);
@@ -47,7 +46,7 @@ int main()
             W(p_accounts);
             break;
         case 'C':
-            C(p_accounts,p_numOfAccounts);
+            C(p_accounts, p_numOfAccounts);
             break;
         case 'I':
             I(p_accounts);
@@ -55,22 +54,21 @@ int main()
         case 'E':
             break;
         case 'P':
-            P(p_accounts,p_numOfAccounts);
+            P(p_accounts, p_numOfAccounts);
             break;
 
         default:
             printf("Please enter function: O , B ,D , W , C , I , P , E \n");
             break;
         }
-
     }
 
     printf("\nREST IN PEACE");
     return 0;
 }
-void O(double (*_accounts)[50][2] , int *_numOfAccounts)
+void O(double (*_accounts)[50][2], int *_numOfAccounts)
 {
-    
+
     if (*_numOfAccounts < 50)
     {
         int whereTo = 0;
@@ -83,23 +81,21 @@ void O(double (*_accounts)[50][2] , int *_numOfAccounts)
             }
         }
 
-        printf("Initial deposit?: ");
+        printf("Please enter amount for deposit: ");
         double toDeposit;
-        //scanf("%lf", &toDeposit);
-        scanf(" %lf", &toDeposit);
-        if  (toDeposit < 0)
+        int check = scanf(" %lf", &toDeposit);
+        if (toDeposit < 0 || check != 1)
         {
             printf("Failed to read the amount \n");
-            memset(&toDeposit ,'\0',sizeof(float));
-                        
+            fflush(stdin);
         }
-        else{
+        else
+        {
             (*_accounts)[whereTo][0] = 1;
             (*_accounts)[whereTo][1] = toDeposit;
             printf("New account to the bank: %d \n", (whereTo + 901));
             ++*_numOfAccounts;
         }
-
     }
     else
     {
@@ -111,17 +107,23 @@ void B(double (*_accounts)[50][2])
 {
     printf("Enter the account number: ");
     int accountNumber;
-    scanf(" %d", &accountNumber);
-    if ((*_accounts)[accountNumber - 901][0]==1)
-    {// the account is active
-    double balance = (*_accounts)[accountNumber-901][1];
-
-    printf("Account number: %d, Balance: %.2lf \n", accountNumber, balance);
-
+    int check = scanf(" %d", &accountNumber);
+    if (check != 1)
+    { // the account is NOT a number
+        printf("Failed to read the account number\n");
+        fflush(stdin);
     }
+    else if ((*_accounts)[accountNumber - 901][0] == 0)
+    {
+        //the account is NOT active
+        printf("This account is closed \n");
+        fflush(stdin);
+    }
+
     else
     {
-        printf("There is not an account: %d \n", accountNumber);
+        double balance = (*_accounts)[accountNumber - 901][1];
+        printf("The balace of account number %d is: %.2lf \n", accountNumber, balance);
     }
 }
 
@@ -130,24 +132,39 @@ void D(double (*_accounts)[50][2])
     printf("Enter the account number: ");
     int accountNumber;
     double amountToDeposit;
-    scanf(" %d", &accountNumber);
+    int checkAccountNumber = scanf(" %d", &accountNumber);
+    if (checkAccountNumber != 1)
+    {
+        printf("Failed to read the account number\n");
+        fflush(stdin);
+    }
+    else if ((*_accounts)[accountNumber - 901][0] == 0)
+    {
+        // the account it NOT active
+        printf("This account is closed \n");
+        fflush(stdin);
+    }
 
-    if ((*_accounts)[accountNumber - 901][0]==1)
-        {
+    else
+    {
         printf("Enter amount to deposit: ");
-        scanf(" %lf", &amountToDeposit);
-        if (amountToDeposit < 0)
+        int checkamoutToDepist = scanf(" %lf", &amountToDeposit);
+        if (checkamoutToDepist != 1)
+        {
+            printf("Failed to read the amount number\n");
+            fflush(stdin);
+        }
+        else if (amountToDeposit < 0)
         {
             printf("You can't deposit negative value! \n");
             return;
         }
-        double balance = (*_accounts)[accountNumber-901][1];
-        (*_accounts)[accountNumber-901][1] = balance + amountToDeposit;
-        printf("New balance: %.2lf \n", (balance + amountToDeposit));
-    }
-    else
-    {
-        printf("There is not an account: %d \n", accountNumber);
+        else
+        {
+            double balance = (*_accounts)[accountNumber - 901][1];
+            (*_accounts)[accountNumber - 901][1] = balance + amountToDeposit;
+            printf("New balance: %.2lf \n", (balance + amountToDeposit));
+        }
     }
 }
 
@@ -156,41 +173,56 @@ void W(double (*_accounts)[50][2])
     printf("Enter the account number: ");
     int accountNumber;
     double amountToWithdrawal;
-    scanf(" %d", &accountNumber);
-    if ((*_accounts)[accountNumber - 901][0]==1)
+    int checkAccountNumber = scanf(" %d", &accountNumber);
+    if (checkAccountNumber!=1){
+        printf("Failed to read the account number\n");
+        fflush(stdin);
+
+    }
+    else if ((*_accounts)[accountNumber - 901][0] == 0){
+        // the account it NOT active
+        printf("There is not an account: %d \n", accountNumber);
+        fflush(stdin);
+    }
+    else if ((*_accounts)[accountNumber - 901][0] == 1)
     {
         printf("Enter amount to withdrawal: ");
-        scanf(" %lf", &amountToWithdrawal);
-        if (amountToWithdrawal < 0)
+        int checkAmountToWithdrawal = scanf(" %lf", &amountToWithdrawal);
+        if (checkAmountToWithdrawal!=1){
+            printf("Failed to read the amount number\n");
+            fflush(stdin);
+        }
+        else if (amountToWithdrawal < 0)
         {
             printf("You can't withdrawal negative value! \n");
             return;
         }
-        if ((*_accounts)[accountNumber - 901][1] < amountToWithdrawal)
+        else if ((*_accounts)[accountNumber - 901][1] < amountToWithdrawal)
         {
-            printf("There is not enought money in the account \n");
+            printf("Cannot withdraw more than the balance \n");
         }
         else
         {
-            double balance = (*_accounts)[accountNumber-901][1];
-            (*_accounts)[accountNumber-901][1] = balance - amountToWithdrawal;
+            double balance = (*_accounts)[accountNumber - 901][1];
+            (*_accounts)[accountNumber - 901][1] = balance - amountToWithdrawal;
             printf("New balance: %.2lf \n", (balance - amountToWithdrawal));
         }
     }
-    else
-    {
-        printf("There is not an account: %d \n", accountNumber);
-    }
+
 }
 
 void C(double (*_accounts)[50][2], int *_numOfAccounts)
 {
     printf("Enter the account number: ");
     int accountNumber;
-    scanf(" %d", &accountNumber);
-    if ((*_accounts)[accountNumber - 901][0]==0)
+    int checkAccountNumber = scanf(" %d", &accountNumber);
+    if(checkAccountNumber!=1){
+        printf("Failed to read the account number\n");
+        fflush(stdin);
+    }
+    else if ((*_accounts)[accountNumber - 901][0] == 0)
     {
-        printf("There is not an account: %d \n", accountNumber);
+        printf("This account is closed \n");
     }
     else
     {
@@ -202,21 +234,20 @@ void C(double (*_accounts)[50][2], int *_numOfAccounts)
 
 void I(double (*_accounts)[50][2])
 {
-    printf("Enter interest rate in percentage: ");
+    printf("Please enter interest rate: ");
     double percent;
-    scanf(" %lf", &percent);
-    if(percent<=0){
-        printf("Not acceptable interest rate!!");
-        return;
+    int checkPercent = scanf(" %lf", &percent);
+    if(checkPercent!=1 || percent<=0){
+        printf("Failed to read the interest rate\n");
+        fflush(stdin);
     }
-    
-    for (int i=0; i<50;i++)
+    else{
+    for (int i = 0; i < 50; i++)
     {
         double balance = (*_accounts)[i][1];
         (*_accounts)[i][1] = balance + (percent / 100) * balance;
-        
     }
-    printf("DONE - interest rate \n");
+    }
 }
 
 void P(double (*_accounts)[50][2], int *_numOfAccounts)
